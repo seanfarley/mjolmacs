d5bf623659a49032b709533e06e8080ec2fd2fe4
Sean Farley <sean@farley.io>
2021-04-23 11:12:20 -0500

    utils: add parser for emacs key sequences

--- mjolmacs-utils.h
+++ mjolmacs-utils.h
@@ -3,9 +3,18 @@
 
 #include <emacs-module.h>
 #import <Cocoa/Cocoa.h>
+#include <objc/NSObjCRuntime.h>
 
 emacs_value Qnil;
 emacs_value Qt;
+
+@interface MjolmacsKey : NSObject {
+}
+
+@property NSUInteger flags;
+@property (strong) NSNumber *key;
+
+@end
 
 void bind_function(emacs_env *env, const char *name, ptrdiff_t min_arity,
                    ptrdiff_t max_arity,
@@ -14,4 +23,7 @@ void bind_function(emacs_env *env, const char *name, ptrdiff_t min_arity,
                    const char *docstring, void *data);
 
 void emacs_error(emacs_env *env, emacs_value err_type, NSString *msg);
+
+NSArray *emacs_parse_keys(emacs_env *env, emacs_value ekb);
+
 #endif // MJOLMACS_UTILS_H_
